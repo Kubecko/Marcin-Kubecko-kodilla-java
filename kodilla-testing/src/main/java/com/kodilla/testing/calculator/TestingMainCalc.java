0c43aaa6dff75961f2c1da14f7e7b87d6d31af62
Kubecko <MKubecko@gmail.com>
2018-05-10 16:00:31 +0200

    Pierwszy test jednostkowy

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/TestingMainCalc.java
@@ -0,0 +1,26 @@
+package com.kodilla.testing;
+
+import com.kodilla.testing.calculator.Calculator;
+
+public class TestingMainCalc {
+    public static void main(String[] args){
+
+        Calculator calcAdd = new Calculator();
+        Calculator calcSubstract = new Calculator();
+
+        int result = calcAdd.add(2,3);
+        int result1 = calcSubstract.substract(3,2);
+
+        if(result > 2){
+            System.out.println("Result for adding: " + result +   " Test OK");
+        } else {
+            System.out.println("Result for adding: " + result + " Error");
+        }
+
+        if(result1 > 3){
+            System.out.println("Result for substraction: " + result1 + " Test OK");
+        } else {
+            System.out.println("Result for substraction: " + result1 + " Error");
+        }
+    }
+}
