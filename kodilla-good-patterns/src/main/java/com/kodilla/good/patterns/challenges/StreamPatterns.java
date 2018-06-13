e0ec882e033e1e9787dc0f49ab2046dbff8cff42
Kubecko <MKubecko@gmail.com>
2018-06-13 16:53:58 +0200

    9.1

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/StreamPatterns.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/StreamPatterns.java
@@ -1,10 +1,14 @@
 package com.kodilla.good.patterns.challenges;
 
+import java.util.stream.Collectors;
+
 public class StreamPatterns {
     public static void main(String[] args) {
         MovieStore movieStore = new MovieStore();
-        movieStore.getMovies().entrySet().stream()
-                  .forEach(System.out::println);
-    }
+        String theMoviesStore = movieStore.getMovies().entrySet().stream()
+                .map(movie -> movie.toString())
+                .collect(Collectors.joining("!"));
 
+        System.out.println(theMoviesStore);
+    }
 }
